7eb802413972e71ee09f194f20014015ffddd88d
walczak-hector <65743857+walczak-hector@users.noreply.github.com>
2023-04-11 16:51:07 -0300

    Agregado de wizard
    Se añadio el formulario wizard

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ function codigoDesc(){
       }
   }
 
-
+/*
   // wizard
   $(function(){
     $("#form-container").formwizard({
@@ -54,4 +54,86 @@ function codigoDesc(){
       next: "Siguiente",
       back: "Anterior"
     });
-  });
+  });*/
+
+
+//Formulario Wizard
+var currentTab = 0;
+document.addEventListener("DOMContentLoaded", function(event) {
+
+
+    showTab(currentTab);
+
+});
+
+function showTab(n) {
+    var x = document.getElementsByClassName("tab");
+    x[n].style.display = "block";
+    if (n != (x.length - 1)) {
+        document.getElementById("imprimir").style.display = "none";
+    }else {
+        document.getElementById("imprimir").style.display = "inline";
+    }
+    if (n == 0) {
+        document.getElementById("prevBtn").style.display = "none";
+    } else {
+        document.getElementById("prevBtn").style.display = "inline";
+    }
+    if (n == (x.length - 2)) {
+        document.getElementById("nextBtn").innerHTML = "Finalizar";
+    } else if(n == (x.length - 1)){
+        document.getElementById("nextBtn").innerHTML = "Enviar pedido";
+        document.getElementById("prevBtn").style.display = "none";
+    }else {
+        document.getElementById("nextBtn").innerHTML = "Siguiente";
+    }
+    fixStepIndicator(n)
+}
+
+function nextPrev(n) {
+    var x = document.getElementsByClassName("tab");
+    if (n == 1 && !validateForm()) return false;
+    x[currentTab].style.display = "none";
+    currentTab = currentTab + n;
+    
+    if (currentTab >= x.length) {
+        document.getElementById("nextprevious").style.display = "none";
+        document.getElementById("all-steps").style.display = "none";
+        document.getElementById("register").style.display = "none";
+        document.getElementById("finalizar").style.display = "block";
+    }
+    showTab(currentTab);
+}
+
+function validateForm() {
+    var x, y, i, valid = true;
+    x = document.getElementsByClassName("tab");
+    y = x[currentTab].getElementsByTagName("input");
+    for (i = 0; i < y.length; i++) {
+        if (y[i].value == "") {
+            y[i].className += " invalid";
+            valid = false;
+        }
+    }
+    if (valid) { document.getElementsByClassName("step")[currentTab].className += " finish"; }
+    return valid;
+}
+
+function fixStepIndicator(n) {
+    var i, x = document.getElementsByClassName("step");
+    for (i = 0; i < x.length; i++) { x[i].className = x[i].className.replace(" active", ""); }
+    x[n].className += " active";
+}
+
+
+$(document).ready(function(){
+    $("#imprimir").click(function(){
+      $('#nombre, #apellido, #email, #telefono, #direccion, #cp, #localidad, #provincia, #mensaje').printThis({
+header: "<h1>Comprobante de contacto</h1>"});
+    })
+  })
+
+
+
+
+
